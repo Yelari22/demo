bbc21a1be63035936e081e3837123544d9ba7055
Prashant <prashant@yelari.com>
2021-01-16 11:00:51 -0500

    + codeowners file

--- githubmappping/githubmappping/DetailViewController.swift
+++ githubmappping/githubmappping/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  githubmappping
 //
 //  Created by Prashant patel on 12/18/20.
@@ -8,23 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     let arrayString = ["a", "b"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
             let button = UIButton(type: .roundedRect)
             button.frame = CGRect(x: 20, y: 100, width: 100, height: 30)
             button.setTitle("Crash", for: [])
             button.addTarget(self, action: #selector(self.crashButton), for: .touchUpInside)
             view.addSubview(button)
-        
-        let detailButton = UIButton(type: .roundedRect)
-        detailButton.frame = CGRect(x: 20, y: 200, width: 100, height: 30)
-        detailButton.setTitle("Detail", for: [])
-        detailButton.addTarget(self, action: #selector(self.detailButtonAction), for: .touchUpInside)
-        view.addSubview(detailButton)
-//        exit(0)
 
     }
 
@@ -35,11 +29,6 @@ class ViewController: UIViewController {
     @IBAction func crashButton() {
 //        preconditionFailure()
         print(arrayString[2])
-
-    }
-    
-    @IBAction func detailButtonAction() {
-        self.navigationController?.present(DetailViewController(), animated: true, completion: nil)
     }
 }
 
